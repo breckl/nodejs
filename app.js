4442b23802db88b6efbb9e15dbaa27ed4d107959
Breck LeSueur <iambreck@gmail.com>
2013-11-06 09:53:20 -0700

    now using mongolabs db

--- app.js
+++ app.js
@@ -17,18 +17,31 @@ app.use(express.static('public'));	// makes all resources in public folder avail
 
 
 // CONNECT TO 'reports' DATABASE
-var server = new mongodb.Server('127.0.0.1', 27017);
+var mongodbURI = 'ds053808.mongolab.com';
+var mongodbPort = 53808;
+
+// var mongoURI = '127.0.0.1';
+// var mongoPort = 27017;
+
+
+
+var server = new mongodb.Server(mongodbURI, mongodbPort);
 
 new mongodb.Db('reports', server, {journal:true}).open(function(err, client){
 
 	if (err) throw err;
 
-	console.log('\033[96m + \033[39m connected to mongodb');
+	client.authenticate('superuser', 'NQu69KyZu7S', function(err, success){
 
-	app.users = new mongodb.Collection(client, 'users');
+		if (err) throw err;
 
-	app.listen(8888, function(){
-		console.log('\033[96m + \033[39m Listening on port 8888');
+		console.log('\033[96m + \033[39m connected to mongodb');
+
+		app.users = new mongodb.Collection(client, 'users');
+
+		app.listen(8888, function(){
+			console.log('\033[96m + \033[39m Listening on port 8888');
+		});
 	});
 
 });
